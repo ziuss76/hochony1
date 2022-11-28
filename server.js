ca874bf6494db2e32116fffeef531eedf3fbd142
김지우 <ziuss76@gmail.com>
2022-11-28 13:35:52 +0900

    router

--- server.js
+++ server.js
@@ -73,4 +73,8 @@ app.get('/getReview', function(요청, 응답){
   });
 });
 
+app.get('*', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/hochony-front/build/index.html'));
+});
+
 // nodemon server.js
