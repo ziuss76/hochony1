17677137fcebc792267f6dae53c9332d27ab7849
김지우 <ziuss76@gmail.com>
2023-07-02 22:25:36 +0900

    Fix: es6 문법으로 변경

--- server.js
+++ server.js
@@ -1,93 +1,112 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const MongoClient = require('mongodb').MongoClient
-const bodyParser = require('body-parser') //요청데이터(body) 해석을 쉽게 도와줌, POST요청 하려면 필요
-app.use(bodyParser.urlencoded({extended : true}));
-require('dotenv').config() //환경변수 라이브러리 dotenv
+import express from "express";
+import path from "path";
+import { MongoClient } from "mongodb";
+import bodyParser from "body-parser"; // 요청데이터(body) 해석을 쉽게 도와줌, POST요청 하려면 필요
+import dotenv from "dotenv";
+import cors from "cors";
+import { fileURLToPath } from "url"; // 파일경로 읽는 fileURLToPath 함수 필요
 
+const app = express();
 app.use(express.json());
-var cors = require('cors');
 app.use(cors()); //nodejs 와 react 사이 ajax 요청 사용하기
+app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url); // 현재 파일의 경로
+const __dirname = path.dirname(__filename); // 현재 파일의 디렉토리 경로
 
 const PORT = process.env.PORT || 8080;
 
-var db; //변수 하나 필요
-MongoClient.connect(process.env.MONGODB_URI, {useUnifiedTopology: true }, function(에러, client){
-     //연결되면 할일
-    if(에러) return console.log(에러) //에러 나오면 콘솔에 띄우기
-    app.listen(PORT, function () {
-      console.log('listening on localhost')
-    }); 
-     db = client.db('ShopData'); //ShopData 이라는 db에 연결
-     console.log('listening on db')
+const uri = process.env.MONGODB_URI;
+const dbName = "ShopData";
+let db;
+
+async function startServer() {
+  try {
+    const client = new MongoClient(uri);
+    await client.connect();
+    db = client.db(dbName);
+    console.log("listening on db");
+    app.listen(PORT, () => {
+      console.log("listening on localhost");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+startServer();
+
+app.use(express.static(path.join(__dirname, "/hochony-front/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/hochony-front/build/index.html"));
 });
 
-app.use(express.static(path.join(__dirname, '/hochony-front/build')));
-
-app.get('/', function (요청, 응답) {
-    응답.sendFile(path.join(__dirname, '/hochony-front/build/index.html'));
-  });
-
-app.get('/content', function(요청, 응답){
-    db.collection('Data').find().toArray(function(에러, 결과){
-      console.log(결과);
-      응답.json(결과);
+app.get("/content", (req, res) => {
+  db.collection("Data")
+    .find()
+    .toArray((err, result) => {
+      // console.log(result);
+      res.json(result);
     });
 });
 
-app.get('/search', function(요청, 응답){
-  let 검색조건 = [
+app.get("/search", (req, res) => {
+  const 검색조건 = [
     {
       $search: {
-        index: 'titleSearch',
+        index: "titleSearch",
         text: {
-          query: 요청.query.value,
-          path: ['title', 'content']  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
-        }
-      }
-    }
-  ] 
-  db.collection('Data').aggregate(검색조건).toArray(function(에러, 결과){
-    console.log(결과);
-    응답.json(결과);
-  })
+          query: req.query.value,
+          path: ["title", "content"], // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+        },
+      },
+    },
+  ];
+  db.collection("Data")
+    .aggregate(검색조건)
+    .toArray((err, result) => {
+      // console.log(result);
+      res.json(result);
+    });
 });
 
-app.get('/getReview', function(요청, 응답){
-  db.collection('Review').find().toArray(function(에러, 결과){
-    console.log(결과);
-    응답.json(결과);
-  });
+app.get("/getReview", (req, res) => {
+  db.collection("Review")
+    .find()
+    .toArray((err, result) => {
+      // console.log(result);
+      res.json(result);
+    });
 });
 
-app.post('/postReview', function(요청, 응답){
-  db.collection('ReviewCount').findOne({name : '게시물개수'}, function(에러, 결과){
-    var 총게시물개수 = 결과.totalPost;
-    db.collection('ReviewCount').updateOne( {name : '게시물개수' } , { $inc : { totalPost : 1 } } , function(에러, 결과){
-      db.collection('Review').insertOne( { _id : (총게시물개수 + 1), 점수 : 요청.body[0], 내용 : 요청.body[1], } , function(){
-        응답.send('전송완료');
+app.post("/postReview", (req, res) => {
+  db.collection("ReviewCount").findOne({ name: "게시물개수" }, (err, result) => {
+    const 총게시물개수 = result.totalPost;
+    db.collection("ReviewCount").updateOne({ name: "게시물개수" }, { $inc: { totalPost: 1 } }, (err, result) => {
+      db.collection("Review").insertOne({ _id: 총게시물개수 + 1, 점수: req.body[0], 내용: req.body[1] }, () => {
+        res.send("전송완료");
       });
-    })
+    });
   });
 });
 
-app.put('/putReview', function(요청, 응답){
-  db.collection('Review').updateOne( {_id : 요청.body[0]}, { $set : {점수 : 요청.body[1], 내용 : 요청.body[2]}, function(){
-    응답.send('수정완료')
-  }})
-})
+app.put("/putReview", (req, res) => {
+  db.collection("Review").updateOne({ _id: req.body[0] }, { $set: { 점수: req.body[1], 내용: req.body[2] } }, () => {
+    res.send("수정완료");
+  });
+});
 
-app.delete('/deleteReview', function(요청, 응답){
-  db.collection('Review').deleteOne(요청.body, function(에러, 결과){
-    console.log('삭제완료')
-  })
-  응답.send('삭제완료')
-})
+app.delete("/deleteReview", (req, res) => {
+  db.collection("Review").deleteOne(req.body, (err, result) => {
+    // console.log("삭제완료");
+  });
+  res.send("삭제완료");
+});
 
-app.get('*', function (요청, 응답) {
-  응답.sendFile(path.join(__dirname, '/hochony-front/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/hochony-front/build/index.html"));
 });
 
 // npm start
-// = nodemon server.js 로 설정함
